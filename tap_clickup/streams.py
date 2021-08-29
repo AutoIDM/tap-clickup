0003de043cbe02f5ca9ad8617d5aed944fc78dab
Derek Visch <visch@users.noreply.github.com>
2021-08-29 16:50:48 -0400

    Spaces (#16)
    * Space stream added
    
    * Update tap.py
    
    * Lint fixed
    
    Co-authored-by: Derek Visch <dvisch@autoidm.com>

--- tap_clickup/streams.py
+++ tap_clickup/streams.py
@@ -1,12 +1,13 @@
 """Stream type classes for tap-clickup."""
 from pathlib import Path
+from typing import Optional
 from tap_clickup.client import ClickUpStream
 
 SCHEMAS_DIR = Path(__file__).parent / Path("./schemas")
 
 
 class TeamsStream(ClickUpStream):
-    """Define custom stream."""
+    """Teams"""
 
     name = "team"
     path = "/team"
@@ -15,3 +16,22 @@ class TeamsStream(ClickUpStream):
     # Optionally, you may also use `schema_filepath` in place of `schema`:
     schema_filepath = SCHEMAS_DIR / "team.json"
     records_jsonpath = "$.teams[*]"
+
+    def get_child_context(self, record: dict, context: Optional[dict]) -> dict:
+        """Return a context dictionary for child streams."""
+        return {
+            "team_id": record["id"],
+        }
+
+
+class SpacesStream(ClickUpStream):
+    """Spaces"""
+
+    name = "space"
+    path = "/team/{team_id}/space"
+    primary_keys = ["id"]
+    replication_key = None
+    # Optionally, you may also use `schema_filepath` in place of `schema`:
+    schema_filepath = SCHEMAS_DIR / "space.json"
+    records_jsonpath = "$.spaces[*]"
+    parent_stream_type = TeamsStream
