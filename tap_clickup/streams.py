9aa2d07fc6f21ce070c82c58774da2d196c63ae9
Mason <masonlopez@damotech.com>
2024-03-22 18:28:57 -0400

    Attempt to use 'at' field from time entry as replication key

--- tap_clickup/streams.py
+++ tap_clickup/streams.py
@@ -35,7 +35,7 @@ class TimeEntries(ClickUpStream):
     name = "time_entries"
     path = "/team/{team_id}/time_entries"
     primary_keys = ["id"]
-    replication_key = None
+    replication_key = "at"
     schema_filepath = SCHEMAS_DIR / "time_entries.json"
     records_jsonpath = "$.data[*]"
     parent_stream_type = TeamsStream
